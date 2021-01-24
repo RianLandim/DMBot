e94f3315495a6aec5689f1a98934a4278b20aa74
Juan Dantas <rianlandim22@gmail.com>
2021-01-23 22:56:14 -0300

    Commands

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
 const Discord = require('discord.js');
-
+require('dotenv').config();
 const client = new Discord.Client();
+const fs = require('fs');
+const path = require('path');
 
-const token = 'Nzk5NzA1NjkxNzQwMDQ1MzM0.YAHd9g.kIIcoq2nneGX_HqOm0JJmtRkxOU'
+const token = process.env.DISCORD_TOKEN
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync(path.join(__dirname, './commands'))
+.filter((filename) => filename.endsWith('.js'))
+
+for(var filename of commandFiles) {
+    const command = require(`./commands/${filename}`);
+
+    client.commands.set(command.name, command);
+}
 
 client.on('ready', () =>{
-    console.log('logged')
+    console.log(`Estou online como ${client.user.username}`)
 });
-client.on('message', msg =>{
-    if(msg.content === 'teste'){
-        msg.reply('Eu estou aqui!')
-    }
-});
-client.on('guildMemberAdd',member =>{
-    const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log')
-    if (!channel) return;
-    channel.send ('Seja Bem vindo ao Servidor, ${member}');
-});
-client.on('message', msg =>{
-    if(msg.content === '!zorkador'){
-        msg.reply('Zorkador vulgo  menino beico')
-    }
-});
-client.on('message', msg =>{
-    if(msg.content === '!dreamy'){
-        msg.reply('undefined')
-    }
-})
-client.on('message', msg => {
-    if(msg.content === '!snz'){
-        msg.reply('O cara do coding!')
-    }
-})
+
 client.login(token);
